aaab6829d21ced70a41fa966ea1e9ca83e405516
ejim favour <favourejim56@gmail.com>
2024-06-09 13:10:20 +0100

    consumed the fund performance data properly

--- components/FundPerformanceCommentary.tsx
+++ components/FundPerformanceCommentary.tsx
@@ -8,10 +8,17 @@ const FundPerformanceCommentary = () => {
   const reports = useAppSelector(
     (state) => state.fundPerformance.performanceReports
   );
+  console.log(reports);
   return (
     <div className="p-[1.5rem]  overflow-y-auto flex-1 flex flex-col justify-between">
       {reports.map((item: any, i: React.Key | null | undefined) => (
-        <NewsTemp key={i} title={item.title} summary={item.summary} />
+        <NewsTemp
+          key={i}
+          title={item.title}
+          summary={item.summary}
+          image={item.docImage}
+          file={item.docFile}
+        />
       ))}
     </div>
   );
