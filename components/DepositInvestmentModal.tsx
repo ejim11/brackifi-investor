1a891fe80d026ebc7812695a7b1f7e3e0eadfda5
ejim favour <favourejim56@gmail.com>
2024-08-03 10:27:45 +0100

    added the hash to the form instructions

--- components/DepositInvestmentModal.tsx
+++ components/DepositInvestmentModal.tsx
@@ -27,7 +27,8 @@ const DepositInvestmentModal = () => {
           </li>
           <li>Make payment to the wallet address.</li>
           <li>
-            Input your wallet address, maximum drawdown and the amount you paid.
+            Input your Transaction hash, wallet address, maximum drawdown and
+            the amount you paid.
           </li>
           <li>Create an investment and wait for it to be verified. </li>
         </ol>
