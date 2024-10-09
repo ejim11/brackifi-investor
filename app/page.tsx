d3332c7b0c917191a755a21512781760a5e76965
ejim favour <favourejim56@gmail.com>
2024-10-09 17:46:48 +0100

    removed the redirection functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,12 @@
 'use client';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useAppDispatch } from '@/hooks/customHook';
-import { investorAction } from '@/slices/investorSlice';
-import { investmentActions } from '@/slices/investmentSlice';
-import { getRoiDetails } from '@/actions/roiValueAction';
 
 export default function Home() {
   const router = useRouter();
 
-  const dispatch = useAppDispatch();
-
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const storedToken = window.localStorage.getItem('investorToken');
-
-      const { name, id, email, phoneNumber, address } = JSON.parse(
-        window.localStorage.getItem('investorDetails') || '{}'
-      );
-
-      const investments = JSON.parse(
-        window.localStorage.getItem('investments') || '[]'
-      );
-
-      dispatch(investorAction.setInvestorToken(storedToken || ''));
-      dispatch(
-        investorAction.setInvestorDetails({
-          name,
-          id,
-          email,
-          phoneNumber,
-          address,
-        })
-      );
-      dispatch(investmentActions.setInvestmentsList(investments));
-   
-
-      if (!storedToken) {
-        router.replace('/auth/login');
-      } else {
-        router.replace(`/dashboard/${name.toLowerCase().split(' ').join('-')}`);
-      }
-    }
+    router.replace('/auth/login');
   }, []);
 
   return <></>;
