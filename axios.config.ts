8ebbb56a9b4a56f24984d88dd5d52042154baf0b
ejim favour <favourejim56@gmail.com>
2024-10-24 14:44:33 +0100

    restructired the axios config

--- axios.config.ts
+++ axios.config.ts
@@ -1,48 +1,30 @@
 import axios from 'axios';
+
+const base =
+  process.env.NEXT_PUBLIC_ENVIROMENT === 'development'
+    ? process.env.NEXT_PUBLIC_LOCAL_HOST
+    : process.env.NEXT_PUBLIC_WEB_HOST;
+
 export const investor = axios.create({
-  baseURL: `${
-    process.env.NEXT_PUBLIC_ENVIROMENT === 'development'
-      ? process.env.NEXT_PUBLIC_LOCAL_HOST
-      : process.env.NEXT_PUBLIC_WEB_HOST
-  }/api/v1/investors`,
+  baseURL: `${base}/api/v1/investors`,
 });
 
 export const reports = axios.create({
-  baseURL: `${
-    process.env.NEXT_PUBLIC_ENVIROMENT === 'development'
-      ? process.env.NEXT_PUBLIC_LOCAL_HOST
-      : process.env.NEXT_PUBLIC_WEB_HOST
-  }/api/v1/reports`,
+  baseURL: `${base}/api/v1/reports`,
 });
 
 export const investments = axios.create({
-  baseURL: `${
-    process.env.NEXT_PUBLIC_ENVIROMENT === 'development'
-      ? process.env.NEXT_PUBLIC_LOCAL_HOST
-      : process.env.NEXT_PUBLIC_WEB_HOST
-  }/api/v1/investments`,
+  baseURL: `${base}/api/v1/investments`,
 });
 
 export const roiValue = axios.create({
-  baseURL: `${
-    process.env.NEXT_PUBLIC_ENVIROMENT === 'development'
-      ? process.env.NEXT_PUBLIC_LOCAL_HOST
-      : process.env.NEXT_PUBLIC_WEB_HOST
-  }/api/v1/roivalue`,
+  baseURL: `${base}/api/v1/roivalue`,
 });
 
 export const fundsPerformance = axios.create({
-  baseURL: `${
-    process.env.NEXT_PUBLIC_ENVIROMENT === 'development'
-      ? process.env.NEXT_PUBLIC_LOCAL_HOST
-      : process.env.NEXT_PUBLIC_WEB_HOST
-  }/api/v1/fundPerformanceCommentaries`,
+  baseURL: `${base}/api/v1/fundPerformanceCommentaries`,
 });
 
 export const businessNews = axios.create({
-  baseURL: `${
-    process.env.NEXT_PUBLIC_ENVIROMENT === 'development'
-      ? process.env.NEXT_PUBLIC_LOCAL_HOST
-      : process.env.NEXT_PUBLIC_WEB_HOST
-  }/api/v1/business-news`,
+  baseURL: `${base}/api/v1/business-news`,
 });
