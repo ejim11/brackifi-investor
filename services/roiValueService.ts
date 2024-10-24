be2d2d826f6b495ce857bfad52bf7918f5c1844d
ejim favour <favourejim56@gmail.com>
2024-10-24 14:47:25 +0100

    removed console.log

--- services/roiValueService.ts
+++ services/roiValueService.ts
@@ -1,7 +1,6 @@
 import { roiValue } from '@/axios.config';
 
 const id = process.env.NEXT_PUBLIC_ROIVALUE_ID;
-console.log(id);
 
 export const getRoiValueData = async () => {
   return await roiValue.get(`/${id}`);
