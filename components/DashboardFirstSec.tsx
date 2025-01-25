568e972e8ac16a30613885bc8bc5d7474be1b40d
ejim11 <favourejim56@gmail.com>
2025-01-25 23:20:49 +0100

    ✅ added dashboardskeleton and dashboard slider

--- components/DashboardFirstSec.tsx
+++ components/DashboardFirstSec.tsx
@@ -8,10 +8,12 @@ import { HiCircleStack } from 'react-icons/hi2';
 import { InvestmentItemType } from '@/app/dashboard/[portfolio]/invest/page';
 import formatDate from '@/utils/dateFormatter';
 import { dateDiffInDays } from '@/utils/helperFns';
-import ShareParamsSlider from './ShareParamsSlider';
+import ShareParamsSlider from './DashboardFirstSecSlider';
 import modifyNum from '@/utils/modifyAmount';
 import { easeIn, motion } from 'framer-motion';
 import { getAllInvestmentsDispatch } from '@/actions/investmentAction';
+import DashboardSkeleton from './skeletons/DashboardSkeleton';
+import DashboardFirstSecSlider from './DashboardFirstSecSlider';
 
 // get the latest roi so we use it to calculate the payout available
 export const getLatestInvRoi = (inv: any) => {
@@ -20,6 +22,19 @@ export const getLatestInvRoi = (inv: any) => {
       0.16
     : 0;
 
+  const investmentTime = dateDiffInDays(
+    new Date(inv.dateCreated).getTime(),
+    new Date(inv.nextPayout).getTime()
+  );
+
+  // if the time has passed the next payout date then investment top amount is the static roi x the difference in active date and next payout
+
+  if (
+    dateDiffInDays(new Date(inv.nextPayout).getTime(), new Date().getTime()) > 0
+  ) {
+    return investmentTime * 0.16;
+  }
+
   // if the roi is greater than the choosen max drawdown for that particular inv then set the max drawdown as the roi
   if (roi >= inv.maximumDrawdown) {
     return inv.maximumDrawdown;
@@ -27,13 +42,10 @@ export const getLatestInvRoi = (inv: any) => {
   return roi.toFixed(2);
 };
 
-const DashboardFirstSec = () => {
-  // dispatch function
-  const dispatch = useAppDispatch();
-
+const DashboardFirstSec = ({ isLoading }: { isLoading: boolean }) => {
   // states
-  const { name, id } = useAppSelector((state) => state.investor.details);
-  const { token } = useAppSelector((state) => state.investor);
+  const { name } = useAppSelector((state) => state.investor.details);
+
   const investments = useAppSelector(
     (state) => state.investments.investments
   ).filter((inv: InvestmentItemType) => inv.investmentState === 'active');
@@ -61,13 +73,29 @@ const DashboardFirstSec = () => {
 
   const totalRoi = () => {
     let staticRoi = 0.16;
+
     const roi = investments
       .filter((inv: InvestmentItemType) => inv.investmentState === 'active')
       .map((inv: InvestmentItemType) => {
+        console.log(inv);
         const dateDiff = dateDiffInDays(
           new Date(inv.activeDate).getTime(),
           new Date().getTime()
         );
+
+        const investmentTime = dateDiffInDays(
+          new Date(inv.dateCreated).getTime(),
+          new Date(inv.nextPayout).getTime()
+        );
+
+        if (
+          dateDiffInDays(
+            new Date(inv.nextPayout).getTime(),
+            new Date().getTime()
+          ) > 0
+        ) {
+          return investmentTime * staticRoi;
+        }
 
         const newRoi = dateDiff * staticRoi;
         if (newRoi >= inv.maximumDrawdown) {
@@ -95,7 +123,7 @@ const DashboardFirstSec = () => {
       .slice()
       .sort(
         (a: any, b: any) =>
-          new Date(a.dateCreated).getTime() - new Date(b.dateCreated).getTime()
+          new Date(b.dateCreated).getTime() - new Date(a.dateCreated).getTime()
       )[0];
 
     return inv;
@@ -109,8 +137,6 @@ const DashboardFirstSec = () => {
       Math.round((inv.amount * getLatestInvRoi(inv)) / 100)
     )
     .reduce((acc, cur) => acc + cur, 0);
-
-  // console.log(totalPayoutAvailable);
 
   const investorPortfolioData = [
     {
@@ -135,6 +161,11 @@ const DashboardFirstSec = () => {
       value:
         investments.length <= 0
           ? 'No Active Investment'
+          : dateDiffInDays(
+              new Date(latestInv.nextPayout).getTime(),
+              new Date().getTime()
+            ) > 0
+          ? 'Available'
           : formatDate(latestInv.nextPayout),
     },
     {
@@ -162,32 +193,37 @@ const DashboardFirstSec = () => {
           Welcome {name.split(' ')[0]},
         </p>
       </div>
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={list}
-        className="grid grid-cols-4 xmd:grid-cols-2 gap-[2rem] font-nunito justify-between font-bold  rounded-lg w-full sm:hidden"
-      >
-        {investorPortfolioData.map((item, i: number) => (
-          <motion.div
-            variants={itemVariant}
-            key={i}
-            transition={{ duration: 0.3, ease: 'easeInOut' }}
-            className=" h-auto rounded-md flex    flex-col items-center  text-center  bg-[#161616] px-[1rem] py-[2rem] shadow-md  text-color-secondary-1 text-[1.7rem] font-bold"
-          >
-            <div className="flex flex-col items-center  mb-[.5rem]">
-              <div className=" text-color-primary-1 mb-[1rem]">
-                {item?.icon}
+      {isLoading ? (
+        <DashboardSkeleton />
+      ) : (
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={list}
+          className="grid grid-cols-4 xmd:grid-cols-2 gap-[2rem] font-nunito justify-between font-bold  rounded-lg w-full sm:hidden"
+        >
+          {investorPortfolioData.map((item, i: number) => (
+            <motion.div
+              variants={itemVariant}
+              key={i}
+              transition={{ duration: 0.3, ease: 'easeInOut' }}
+              className=" h-auto rounded-md flex    flex-col items-center  text-center  bg-[#161616] px-[1rem] py-[2rem] shadow-md  text-color-secondary-1 text-[1.7rem] font-bold"
+            >
+              <div className="flex flex-col items-center  mb-[.5rem]">
+                <div className=" text-color-primary-1 mb-[1rem]">
+                  {item?.icon}
+                </div>
+                <p className="text-[1.8rem] font-semibold text-color-primary-1 uppercase mb-[1rem]">
+                  {item.title}
+                </p>
+                <p>{item.value}</p>
               </div>
-              <p className="text-[1.8rem] font-semibold text-color-primary-1 uppercase mb-[1rem]">
-                {item.title}
-              </p>
-              <p>{item.value}</p>
-            </div>
-          </motion.div>
-        ))}
-      </motion.div>
-      <ShareParamsSlider shareParams={investorPortfolioData} />
+            </motion.div>
+          ))}
+        </motion.div>
+      )}
+
+      {!isLoading && <DashboardFirstSecSlider data={investorPortfolioData} />}
     </motion.div>
   );
 };
