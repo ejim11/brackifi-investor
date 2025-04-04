21d2cca3a6c867b0c8f06d497644f1204243898a
ejim11 <favourejim56@gmail.com>
2025-04-04 20:08:26 +0100

    ⚡️ improved the validation principle

--- utils/inputValidators.ts
+++ utils/inputValidators.ts
@@ -1,26 +1,26 @@
 export const registrationOption = {
-  name: {
-    required: 'Name is required',
+  name: (required: boolean) => ({
+    required: required ? 'Name is required' : false,
     minLength: {
       value: 3,
       message: 'Name must have at least 3 characters',
     },
     maxLength: {
-      value: 20,
-      message: 'Name cannot be greater than 20 characters',
+      value: 100,
+      message: 'Name cannot be greater than 100 characters',
     },
     // pattern: {
     //   value: /^[A-Z][a-z]+\s[A-Z][a-z]+$/i,
     //   message: 'Full name is required',
     // },
-  },
-  email: {
-    required: 'Email is required',
+  }),
+  email: (required: boolean) => ({
+    required: required ? 'Email is required' : false,
     pattern: {
       value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
       message: 'Valid email address is required',
     },
-  },
+  }),
   password: {
     required: 'Password is required',
     minLength: {
@@ -32,15 +32,15 @@ export const registrationOption = {
       message: 'Password cannot be greater than 20 characters',
     },
   },
-  phoneNumber: {
-    required: 'Phone number is required',
+  phoneNumber: (required: boolean) => ({
+    required: required ? 'Phone number is required' : false,
     pattern: {
       value: /(?:\+?(\d{1,3}))?[\s.-]?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/,
       message: 'Valid phone number is required',
     },
-  },
-  address: {
-    required: 'Address is required',
+  }),
+  address: (required: boolean) => ({
+    required: required ? 'Address is required' : false,
     minLength: {
       value: 2,
       message: 'Address name must have at least 2 characters',
@@ -49,7 +49,7 @@ export const registrationOption = {
       value: 150,
       message: 'Address name cannot be greater than 150 characters',
     },
-  },
+  }),
   ubitexId: {
     required: false,
     minLength: {
